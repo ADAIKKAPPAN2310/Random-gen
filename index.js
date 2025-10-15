82eb0850b2d91413fca6f3608ecd5cf3280c75c1
ADAIKKAPPAN2310 <adaikkappan838@gmail.com>
2025-10-15 22:17:11 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,16 @@
+const randomColor = function () {
+  let color = "#";
+  const chars = "0123456789ABCDEF";
+  for (let i = 0; i < 6; i++)
+    color += chars[Math.floor(Math.random() * chars.length)];
+  return color;
+};
+
+const randomPassword = function (len = 8) {
+  let password = "";
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%^&*";
+  for (let i = 0; i < Number(len); i++)
+    password += chars[Math.floor(Math.random() * chars.length)];
+  return password;
+};
